9f46a34dc54c83db2b9afe0468c0819f480bf0be
Al-Amin Islam <jimalamin7@gmail.com>
2026-02-22 07:05:57 +0600

    zod validation

--- PH-HealthCare-Backend/src/app/modules/user/user.routes.ts
+++ PH-HealthCare-Backend/src/app/modules/user/user.routes.ts
@@ -1,8 +1,14 @@
 import { Router } from "express";
 import { userController } from "./user.controller";
+import { zodValidation } from "../../middleware/zodValidation";
+import { createDoctorZodSchema } from "./user.validation";
 
 const route = Router();
 
-route.post("/create-doctor", userController.createDoctor);
+route.post(
+  "/create-doctor",
+  zodValidation(createDoctorZodSchema),
+  userController.createDoctor,
+);
 
 export const userRoute = route;
