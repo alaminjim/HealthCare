c5beb908ef513360d349e2c4b217df859dcc939d
Al-Amin Islam <jimalamin7@gmail.com>
2026-03-10 17:37:12 +0600

    schedule crud

--- PH-HealthCare-Backend/src/app/modules/schedule/schdule.controller.ts
+++ PH-HealthCare-Backend/src/app/modules/schedule/schdule.controller.ts
@@ -2,15 +2,66 @@ import { Request, Response } from "express";
 import catchFn from "../../shared/catchFn";
 import { StatusCodes } from "http-status-codes";
 import { scheduleService } from "./schedule.service";
+import { IQueryParams } from "../../interface/queryBuilders.i";
 
 const createSchedule = catchFn(async (req: Request, res: Response) => {
   const payload = req.body;
 
   const result = await scheduleService.createSchedule(payload);
 
-  res.status(StatusCodes.CREATED).json({ success: true, data: result });
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    data: result,
+    message: "Schedule created successful",
+  });
+});
+
+const getAllSchedule = catchFn(async (req: Request, res: Response) => {
+  const query = req.query;
+  const result = await scheduleService.getAllSchedules(query as IQueryParams);
+
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: result,
+    message: "Schedule get successful",
+  });
+});
+
+const getScheduleById = catchFn(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const schedule = await scheduleService.getScheduleById(id as string);
+  res.status(StatusCodes.OK).json({
+    success: true,
+    data: schedule,
+    message: "Schedule get successful",
+  });
+});
+
+const updateSchedule = catchFn(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  const payload = req.body;
+  const result = await scheduleService.updateSchedule(id as string, payload);
+  res.status(StatusCodes.CREATED).json({
+    success: true,
+    data: result,
+    message: "Schedule update successful",
+  });
+});
+
+const deleteSchedule = catchFn(async (req: Request, res: Response) => {
+  const { id } = req.params;
+  await scheduleService.deleteSchedule(id as string);
+  res.status(StatusCodes.OK).json({
+    success: true,
+
+    message: "Schedule delete successful",
+  });
 });
 
 export const scheduleController = {
   createSchedule,
+  getAllSchedule,
+  getScheduleById,
+  updateSchedule,
+  deleteSchedule,
 };
--- PH-HealthCare-Backend/src/app/modules/schedule/schedule.route.ts
+++ PH-HealthCare-Backend/src/app/modules/schedule/schedule.route.ts
@@ -1,5 +1,38 @@
 import { Router } from "express";
+import { Role } from "../../../generated/prisma/enums";
+import { authMiddleware } from "../../middleware/auth";
+import { zodValidation } from "../../middleware/zodValidation";
+import { scheduleController } from "./schdule.controller";
+import { ScheduleValidation } from "./schdule.validation";
 
-const route = Router();
+const router = Router();
 
-export const scheduleRoute = route;
+router.post(
+  "/",
+  authMiddleware(Role.ADMIN, Role.SUPER_ADMIN),
+  zodValidation(ScheduleValidation.createScheduleZodSchema),
+  scheduleController.createSchedule,
+);
+router.get(
+  "/",
+  authMiddleware(Role.ADMIN, Role.SUPER_ADMIN, Role.DOCTOR),
+  scheduleController.getAllSchedule,
+);
+router.get(
+  "/:id",
+  authMiddleware(Role.ADMIN, Role.SUPER_ADMIN, Role.DOCTOR),
+  scheduleController.getScheduleById,
+);
+router.patch(
+  "/:id",
+  authMiddleware(Role.ADMIN, Role.SUPER_ADMIN),
+  zodValidation(ScheduleValidation.updateScheduleZodSchema),
+  scheduleController.updateSchedule,
+);
+router.delete(
+  "/:id",
+  authMiddleware(Role.ADMIN, Role.SUPER_ADMIN),
+  scheduleController.deleteSchedule,
+);
+
+export const scheduleRoute = router;
